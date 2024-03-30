234351554c418e5945059d6bbd7fff0197844879
josago97 <josago2009@hotmail.es>
2024-03-31 00:45:00 +0100

    Improved navigation

--- src/Eurovision.WebApp/Navigator.cs
+++ src/Eurovision.WebApp/Navigator.cs
@@ -6,21 +6,17 @@ namespace Eurovision.WebApp;
 
 public class Navigator : IDisposable
 {
-    private static readonly string[] BASE_URL = ["junior", "senior" ];
+    private const char URL_SEPARATOR = '/';
+    private static readonly string[] BASE_URL = ["junior", "senior"];
 
     private readonly NavigationManager _navigationManager;
     private readonly IJSRuntime _jsRuntime;
-    private readonly List<string> _history;
+    private readonly List<Path> _history;
 
     /// <summary>
     /// Returns true if it is possible to navigate to the previous url.
     /// </summary>
     public bool CanNavigateBack => _history.Count > 1;
-
-    /// <summary>
-    /// Returns the previous url.
-    /// </summary>
-    public string LastUrl => CanNavigateBack ? _history[^2] : string.Empty;
 
     /// <summary>
     /// An event that fires when the navigation location has changed.
@@ -31,9 +27,25 @@ public class Navigator : IDisposable
     {
         _navigationManager = navigationManager;
         _jsRuntime = jsRuntime;
-        _history = new List<string>();
-        AddUrl(_navigationManager.Uri);
+        _history = new List<Path>();
         _navigationManager.LocationChanged += OnLocationChanged;
+
+        Init();
+    }
+
+    // Search in the initial url and add the previous pages to the history
+    private void Init()
+    {
+        string currentUrl = _navigationManager.ToBaseRelativePath(_navigationManager.Uri);
+        int separatorIndex = -1;
+
+        while ((separatorIndex = currentUrl.IndexOf(URL_SEPARATOR, separatorIndex + 1)) != -1)
+        {
+            string url = currentUrl.Substring(0, separatorIndex);
+            AddUrl(url, false, true);
+        }
+
+        AddUrl(currentUrl, true, true);
     }
 
     /// <summary>
@@ -50,37 +62,51 @@ public class Navigator : IDisposable
     /// </summary>
     public async Task GoBackAsync()
     {
-        await _jsRuntime.InvokeVoidAsync("history.back");
+        if (!CanNavigateBack) return;
+
+        Path lastPath = _history[^2];
+
+        if (lastPath.CanBackBrowser)
+        {
+            await _jsRuntime.InvokeVoidAsync("history.back");
+        }
+        else
+        {
+            NavigateTo(lastPath.Url);
+        }
     }
 
     private void OnLocationChanged(object sender, LocationChangedEventArgs e)
     {
-        AddUrl(e.Location);
+        AddUrl(e.Location, true, false);
         LocationChanged?.Invoke(this, e);
     }
 
-    private void AddUrl(string absoluteUrl)
+    private void AddUrl(string url, bool canBrowserBack, bool isRelative)
     {
-        string relative = _navigationManager.ToBaseRelativePath(absoluteUrl);
+        string relativeUrl = isRelative ? url : _navigationManager.ToBaseRelativePath(url);
 
-        if (BASE_URL.Contains(relative))
+        if (string.IsNullOrEmpty(relativeUrl) || BASE_URL.Contains(relativeUrl))
         {
             _history.Clear();
         }
+
+        int relativeIndex = _history.FindIndex(path => path.Url == relativeUrl);
+
+        if (relativeIndex != -1)
+        {
+            _history.RemoveRange(relativeIndex, _history.Count - relativeIndex - 1);
+        }
         else
         {
-            int relativeIndex = _history.IndexOf(relative);
-            if (relativeIndex != -1)
-            {
-                _history.RemoveRange(relativeIndex, _history.Count - relativeIndex);
-            }
+            _history.Add(new Path(relativeUrl, canBrowserBack));
         }
-
-        _history.Add(relative);
     }
 
     public void Dispose()
     {
         _navigationManager.LocationChanged -= OnLocationChanged;
     }
+
+    private record Path(string Url, bool CanBackBrowser);
 }
